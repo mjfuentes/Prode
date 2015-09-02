8a27f46ea6c71062e56480abc265ddc20281fef6
Matias Fuentes <matias.fuentes@globallogic.com>
2015-09-02 01:41:56 -0300

    removed comments

--- test/controllers/matches_controller_test.rb
+++ test/controllers/matches_controller_test.rb
@@ -1,8 +1,5 @@
 require 'test_helper'
 
 class MatchesControllerTest < ActionController::TestCase
-  test "save_matchday" do
-    matchday = Matchday.find_by started:true
-    assert Match.new(matchday_id:matchday.id,home_team: "River", away_team: "Boca").save
-  end
+
 end
--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class PlayersControllerTest < ActionController::TestCase
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
+
 
 end
